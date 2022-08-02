98e9a99662d816156bca914521ea6d520d8c7c9d
tusarr10 <tusarr10@gmail.com>
2022-08-02 21:21:42 +0530

    Add project files.

---
+++ DataBaseHelper/EmployeeIn/ClsEmplInfo.vb
@@ -0,0 +1,17 @@
+﻿
+Public Class ClsEmplInfo
+
+    Private mUserId As String
+    Private mName As String
+    Private mFather As String
+    Private mDOB As String
+    Private mGender As String
+    Private mAddress As String
+    Private mMobile As String
+    Private mMobile1 As String
+    Private mEmail As String
+    Private mBlood As String
+    Private 
+
+
+End Class
